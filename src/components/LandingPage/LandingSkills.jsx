57ee13412935ba42504d992ef679238a6421e309
Mr. Jimwell <ibayjimwell@gmail.com>
2023-08-30 19:55:51 +0800

    Update LandingSkills.jsx

--- src/components/LandingPage/LandingSkills.jsx
+++ src/components/LandingPage/LandingSkills.jsx
@@ -5,15 +5,17 @@ function LandingSkills() {
   return (
     <>
         <div className="container">
-              <div className="grid grid-cols-3 grid-rows-3 gap-4 mb-20">
+              <div className="grid grid-cols-4 grid-rows-2 gap-4 mb-20">
 
                 <Label text='JavaScript' background="bg-yellow-500"></Label>
-                <Label text='Python' background="bg-cyan-800"></Label>
-                <Label text='React' background="bg-blue-800"></Label>
-                
                 <Label text='Next.js' background="bg-zinc-900"></Label>
+                <Label text='Python' background="bg-cyan-800"></Label>
+                <Label text='Django' background='bg-green-800'></Label>
+
+                <Label text='React' background="bg-blue-800"></Label>
                 <Label text='Tailwind' background='bg-indigo-900'></Label>
                 <Label text='Boostrap' background='bg-violet-700'></Label>
+                <Label text='Comming Soon' background='bg-stone-800'></Label>
 
               </div>
         </div>
