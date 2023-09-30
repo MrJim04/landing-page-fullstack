db7e38530c7c2fc4d8fab80fe2dad4228403d924
MrJim04 <125801883+MrJim04@users.noreply.github.com>
2023-09-30 18:06:54 +0800

    Update data.js it's have typo

--- data/data.js
+++ data/data.js
@@ -6,7 +6,7 @@ const data = {
     },
 
     "content": {
-        "introduction": "Hi, I'm Jimwell Ibay, an IT professional with expertise in web technologies. I have a strong foundation in creating modern minimalist websites or web apps. I believe that I can contribute and gain skills and experience in your organization or company. Click the proceed button to check out my smaple projects.",
+        "introduction": "Hi, I'm Jimwell Ibay, an IT professional with expertise in web technologies. I have a strong foundation in creating modern minimalist websites or web apps. I believe that I can contribute and gain skills and experience in your organization or company. Click the proceed button to check out my sample projects.",
         "motto": ["I will make you accessible digitally", "Tell me what the problem and I will fix the problem"]
     },
 
